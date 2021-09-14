8586a5868b226b939f504a25bb1c5e998bdaec27
Cbturtle2 <charleslbeam@gmail.com>
2021-09-14 16:51:49 -0400

    Added content to contact me, resume, and projects

--- app.py
+++ app.py
@@ -9,3 +9,19 @@ def index():
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
+@app.route('/jobs')
+def jobs():
+    return render_template('jobs.html')
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
